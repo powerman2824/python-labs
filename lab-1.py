1f88dfbbb9f4b9ae1a9c5f5b08c38c8f09b40b3b
Joseph Cafarelli <64694539+powerman2824@users.noreply.github.com>
2021-01-30 11:34:51 -0500

    Lab 1/30/2021
    First Lab

---
+++ lab-1.py
@@ -0,0 +1,94 @@
+>>> print("Hello World")
+Hello World
+>>> print("Hello\nWorld")
+Hello
+World
+>>> hello_1 = "Hello There"
+>>> name_1 = "Bob"
+>>> print(hello_1 + " my name is " + name_1)
+Hello There my name is Bob
+>>> hello_1 = "Hello there"
+>>> print(hello_1 + " my name is " + name_1)
+Hello there my name is Bob
+>>> int_1 = 2
+>>> flt_1 = 2.0
+>>> type(int_1)
+<class 'int'>
+>>> type(flt_1)
+<class 'float'>
+>>> mynum = 2
+>>> yournum = 1
+>>> print(mynum + yournum)
+3
+>>> yournum = 3
+>>> print(mynum + yournum)
+5
+>>> print(2 ** 2)
+4
+>>> print(2 ** 3)
+8
+>>> print(2 ** 4)
+16
+>>> print(2 ** 5)
+32
+>>> my_team = print(27 % 4)
+3
+>>> my_team
+>>> type(my_team)
+<class 'NoneType'>
+>>> def my_team()
+SyntaxError: invalid syntax
+>>> def my_team():
+	print(27 % 4)
+
+	
+>>> my_team
+<function my_team at 0x7f1c8d9c1a60>
+>>> my_team()
+3
+>>> type(my_team())
+3
+<class 'NoneType'>
+>>> my_team()
+3
+>>> age = 10
+>>> type(age)
+<class 'int'>
+>>> type(str(age))
+<class 'str'>
+>>> newnum = 5
+>>> print(newnum)
+5
+>>> newnum += 5
+>>> print(newnum)
+10
+>>> print(2**4)
+16
+>>> num = 12 + 30
+>>> num * 5
+210
+>>> print(num)
+42
+>>> num == 10
+False
+>>> num == 42
+True
+>>> num = 10
+>>> print(num)
+10
+>>> num == 10
+True
+>>> 14 % 4
+2
+>>> q_1 = input("How are today")
+How are todaygood
+>>> print(q_1)
+good
+>>> q_1 = input("How are today\n-->")
+How are today
+-->Good
+>>> q_1 = input("How are today?\n--> ")
+How are today?
+--> Good
+>>> print(q_1)
+Good
